21427dfe9e6424d258819d5db0c523e8dd5addab
stevenli2020 <root@ezmote.com>
2023-11-17 11:03:04 +0800

    update

--- docker-image/entrypoint.sh
+++ docker-image/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 if [ ! -f /app/run.py ]; then
-    echo "File not found!"
+    curl https://raw.githubusercontent.com/stevenli2020/XCRF868_Proxy_Service/master/source/run.py > /app/run.py
 fi
 python3 /app/run.py
